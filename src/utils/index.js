5a48cf8da4565e9b195e3c94b33e9cefe4882cee
Eloagbawe <eloagbawe@gmail.com>
2024-07-10 15:23:12 -0400

    update input validation functions

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,13 +1,11 @@
 export const validateEmailInput = (email) => {
   const emailPattern =
     /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
-  
-  return (emailPattern.test(email));
-}
 
+  return emailPattern.test(email);
+};
 
 export const validateLoginInput = (input) => {
-
   const { email, password } = input;
   let validatedInput = { email, password };
 
@@ -17,8 +15,8 @@ export const validateLoginInput = (input) => {
     validatedInput.email = {
       ...email,
       error: true,
-      message: 'Email is required'
-    }
+      message: "Email is required",
+    };
   }
 
   if (!password.value) {
@@ -27,8 +25,8 @@ export const validateLoginInput = (input) => {
     validatedInput.password = {
       ...password,
       error: true,
-      message: 'Password is required'
-    }
+      message: "Password is required",
+    };
   }
 
   if (email.value && !validateEmailInput(email.value)) {
@@ -36,26 +34,32 @@ export const validateLoginInput = (input) => {
     validatedInput.email = {
       ...email,
       error: true,
-      message: 'Please provide a valid email address'
-    }
+      message: "Please provide a valid email address",
+    };
   }
 
   return validatedInput;
-  
-}
+};
 
 export const validateSignupInput = (input) => {
   const { first_name, last_name, email, password, confirm_password } = input;
 
-  const validatedInput = {error: false, first_name, last_name, email, password, confirm_password};
+  const validatedInput = {
+    error: false,
+    first_name,
+    last_name,
+    email,
+    password,
+    confirm_password,
+  };
 
   if (!first_name.value) {
     validatedInput.error = true;
     validatedInput.first_name = {
       ...first_name,
       error: true,
-      message: 'First name is required'
-    }
+      message: "First name is required",
+    };
   }
 
   if (!last_name.value) {
@@ -63,8 +67,8 @@ export const validateSignupInput = (input) => {
     validatedInput.last_name = {
       ...last_name,
       error: true,
-      message: 'Last name is required'
-    }
+      message: "Last name is required",
+    };
   }
 
   if (!email.value) {
@@ -72,8 +76,8 @@ export const validateSignupInput = (input) => {
     validatedInput.email = {
       ...email,
       error: true,
-      message: 'Email is required'
-    }
+      message: "Email is required",
+    };
   }
 
   if (!password.value) {
@@ -81,8 +85,8 @@ export const validateSignupInput = (input) => {
     validatedInput.password = {
       ...password,
       error: true,
-      message: 'Password is required'
-    }
+      message: "Password is required",
+    };
   }
 
   if (!confirm_password.value) {
@@ -90,17 +94,17 @@ export const validateSignupInput = (input) => {
     validatedInput.confirm_password = {
       ...confirm_password,
       error: true,
-      message: 'Confirm password is required'
-    }
+      message: "Confirm password is required",
+    };
   }
 
-  if (password.value && (password.value !== confirm_password.value)) {
+  if (password.value && password.value !== confirm_password.value) {
     validatedInput.error = true;
     validatedInput.confirm_password = {
       ...confirm_password,
       error: true,
-      message: 'Passwords don\'t match'
-    }
+      message: "Passwords don't match",
+    };
   }
 
   if (email.value && !validateEmailInput(email.value)) {
@@ -108,13 +112,13 @@ export const validateSignupInput = (input) => {
     validatedInput.email = {
       ...email,
       error: true,
-      message: 'Please provide a valid email address'
-    }
+      message: "Please provide a valid email address",
+    };
   }
 
   return validatedInput;
-}
+};
 
-export const capitalizeInitials = (first_name, last_name) => {  
+export const capitalizeInitials = (first_name, last_name) => {
   return `${first_name[0].toUpperCase()}${last_name[0].toUpperCase()}`;
-}
+};
