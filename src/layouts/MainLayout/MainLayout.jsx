e299fcfec49cb9d15341242ee716beeadb66a2c0
Eloagbawe <eloagbawe@gmail.com>
2024-07-10 18:33:32 -0400

    update main layout

--- src/layouts/MainLayout/MainLayout.jsx
+++ src/layouts/MainLayout/MainLayout.jsx
@@ -1,23 +1,23 @@
-import './MainLayout.scss';
-import NavBar from '../../components/NavBar/NavBar';
+import "./MainLayout.scss";
+import NavBar from "../../components/NavBar/NavBar";
 import { Divider } from "@chakra-ui/react";
-import Footer from '../../components/Footer/Footer';
+import Footer from "../../components/Footer/Footer";
 
-const MainLayout = ({children}) => {
+const MainLayout = ({ children }) => {
   return (
-    <main className='main-layout'>
-      <section className='main-layout__nav'>
-        <NavBar/>
+    <main className="main-layout">
+      <section className="main-layout__nav">
+        <NavBar />
       </section>
-      <Divider/>
-      <section className='main-layout__content'>
+      <Divider />
+      <section className="main-layout__content px-6 md:px-12 my-4 md:my-8 xl:my-16">
         {children}
       </section>
       <section>
-        <Footer/>
+        <Footer />
       </section>
     </main>
-  )
-}
+  );
+};
 
-export default MainLayout
+export default MainLayout;
