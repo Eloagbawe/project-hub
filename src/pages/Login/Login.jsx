3c2c572bf9d1e1f6285345fb2c0c205a3e2dc869
Eloagbawe <eloagbawe@gmail.com>
2024-07-10 12:45:54 -0400

    implement login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -3,15 +3,87 @@ import MainLayout from "../../layouts/MainLayout/MainLayout";
 import visibilityIcon from "../../assets/icons/visibility.svg";
 import visibilityOffIcon from "../../assets/icons/visibility_off.svg";
 import { Link } from "react-router-dom";
-import { useState } from "react";
+import { useState, useContext, useEffect } from "react";
+import { validateLoginInput } from "../../utils";
+import projectHubApi from "../../utils/projectHubApi";
+import { UserContext } from "../../contexts/userContext";
+import { Spinner } from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
+import { AlertContext } from "../../contexts/alertContext";
 
 const Login = () => {
+  const { user, setUser } = useContext(UserContext);
+  const { displayAlert } = useContext(AlertContext);
+  const navigate = useNavigate();
+  const [loginLoading, setLoginLoading] = useState(false);
   const [viewPassword, setViewPassword] = useState(false);
+  const [input, setInput] = useState({
+    email: {
+      value: "",
+      error: false,
+    },
+    password: {
+      value: "",
+      error: false,
+    },
+  });
 
+  const { email, password } = input;
 
-  const handleLogin = (e) => {
+  const handleInputChange = (e) => {
+    setInput({
+      ...input,
+      [e.target.name]: {
+        value: e.target.value,
+        error: false,
+      },
+    });
+  };
+
+  const handleLogin = async (e) => {
     e.preventDefault();
-  }
+
+    const validatedInput = validateLoginInput(input);
+
+    setInput({
+      email: validatedInput.email,
+      password: validatedInput.password,
+    });
+
+    if (validatedInput.error) {
+      return;
+    }
+    setLoginLoading(true);
+
+    try {
+      const { data } = await projectHubApi.login({
+        email: email.value,
+        password: password.value,
+      });
+
+      localStorage.setItem("user", JSON.stringify(data.user));
+      localStorage.setItem("token", JSON.stringify(data.token));
+      setUser(data.user);
+      setLoginLoading(false);
+      displayAlert({
+        text: data.message,
+      });
+      navigate("/");
+    } catch (err) {
+      setLoginLoading(false);
+      displayAlert({
+        text: err.response?.data?.message,
+        status: "error",
+      });
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    if (user) {
+      navigate("/");
+    }
+  }, [navigate, user]);
 
   return (
     <MainLayout>
@@ -25,19 +97,31 @@ const Login = () => {
             <input
               type="text"
               id="email"
-              className="login__input border rounded-lg py-2 px-3 w-full md:w-6/12 xl:w-5/12"
+              name="email"
+              value={email.value}
+              onChange={handleInputChange}
+              className={`login__input border ${
+                email.error && "login__input--error"
+              } rounded-lg py-2 px-3 w-full md:w-6/12 xl:w-5/12`}
             />
           </div>
           <div className="my-6 md:my-8 xl:my-10">
             <label htmlFor="password" className="login__label block mb-1">
               Password:
             </label>
-            <div className="relative login__input-container border rounded-lg  w-full md:w-6/12 xl:w-5/12">
+            <div
+              className={`relative login__input-container border ${
+                password.error && "login__input-container--error"
+              } rounded-lg  w-full md:w-6/12 xl:w-5/12`}
+            >
               <input
-                  type={viewPassword ? "text" : "password"}
-                  id="password"
-                  className="login__input w-full py-2 px-3 rounded-lg"
-                />
+                type={viewPassword ? "text" : "password"}
+                id="password"
+                name="password"
+                value={password.value}
+                onChange={handleInputChange}
+                className="login__input w-full py-2 px-3 rounded-lg"
+              />
               {viewPassword ? (
                 <button type="button" onClick={() => setViewPassword(false)}>
                   <img
@@ -55,12 +139,22 @@ const Login = () => {
                   />
                 </button>
               )}
-              
             </div>
           </div>
-          
-          <button className="login__btn rounded-full px-8 md:px-16 py-2 w-full my-2 md:w-fit hover:opacity-90 transition duration-500">Log In</button>
-          <p className="login__text my-1">Don&apos;t have an account? <Link to="/signup" className="login__text--link hover:opacity-90 transition duration-300">Sign Up</Link></p>
+
+          <button className="login__btn rounded-full px-8 md:px-16 py-2 w-full my-2 md:w-fit hover:opacity-90 transition duration-500 flex gap-2 items-center">
+            Log In
+            {loginLoading && <Spinner size="xs" />}
+          </button>
+          <p className="login__text my-1">
+            Don&apos;t have an account?{" "}
+            <Link
+              to="/signup"
+              className="login__text--link hover:opacity-90 transition duration-300"
+            >
+              Sign Up
+            </Link>
+          </p>
         </form>
       </section>
     </MainLayout>
