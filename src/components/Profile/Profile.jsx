2c127586a2e7e4c0c56f310fd019fbffba426609
Eloagbawe <eloagbawe@gmail.com>
2024-07-10 18:32:25 -0400

    update profile component

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,9 +1,9 @@
 import "./Profile.scss";
 
-const Profile = ({ name }) => {
+const Profile = ({ name, small }) => {
   return (
-    <div className="profile rounded-full w-8 h-8 flex items-center justify-center overflow-hidden">
-      <span className="profile__text">{name}</span>
+    <div className={`profile rounded-full w-8 h-8 flex items-center justify-center overflow-hidden ${small && 'profile--small w-6 h-6'}`}>
+      <span className={`profile__text ${small && 'profile__text--small'}`}>{name}</span>
     </div>
   );
 };
