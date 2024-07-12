0289e2287f65a6706d369cd50e5d3b766f8b6061
Eloagbawe <eloagbawe@gmail.com>
2024-07-11 22:08:35 -0400

    update profile and project item components

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -2,8 +2,14 @@ import "./Profile.scss";
 
 const Profile = ({ name, small }) => {
   return (
-    <div className={`profile rounded-full w-8 h-8 flex items-center justify-center overflow-hidden ${small && 'profile--small w-6 h-6'}`}>
-      <span className={`profile__text ${small && 'profile__text--small'}`}>{name}</span>
+    <div
+      className={`profile rounded-full flex items-center justify-center overflow-hidden ${
+        small && "profile--small"
+      }`}
+    >
+      <span className={`profile__text ${small && "profile__text--small"}`}>
+        {name}
+      </span>
     </div>
   );
 };
--- src/components/ProjectItem/ProjectItem.jsx
+++ src/components/ProjectItem/ProjectItem.jsx
@@ -1,18 +1,22 @@
 import "./ProjectItem.scss";
 import { Divider } from "@chakra-ui/react";
 import Profile from "../Profile/Profile";
+import { capitalizeInitials } from "../../utils";
+import moment from "moment";
 
 const ProjectItem = ({ project }) => {
   return (
     <div className="project rounded-sm">
       <div className="project__content px-2 py-4 flex flex-wrap gap-3 justify-between align-center">
-        <p className="project__name">{project?.title}</p>
-        <div className="project__details flex flex-wrap gap-3 items-center">
+        <div className="w-full md:w-fit">
+          <p className="project__name">{project?.title}</p>
+        </div>
+        <div className="project__details flex flex-wrap gap-5 items-center">
           <div className="project__lead-container flex gap-1 items-center">
-            <Profile name="OI" small={true} />
-            <p className="project__lead">{project?.manager}</p>
+            <Profile name={capitalizeInitials(project?.manager_first_name, project?.manager_last_name)} small={true} />
+            <p className="project__lead">{project?.manager_first_name} {project?.manager_last_name}</p>
           </div>
-          <p className="project__timestamp">Last Updated: {project.updated_at}</p>
+          <p className="project__timestamp">Last Updated: {moment(project.updated_at).format('MMMM Do YYYY')}</p>
         </div>
       </div>
 
