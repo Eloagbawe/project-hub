9e59fcbb1aa768671854737217c9fd0bd47e7362
Eloagbawe <eloagbawe@gmail.com>
2024-07-11 22:14:16 -0400

    update landing page and project list page

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,19 +1,26 @@
 import "./Landing.scss";
 import MainLayout from "../../layouts/MainLayout/MainLayout";
+import { Link } from "react-router-dom";
+import { UserContext } from "../../contexts/userContext";
+import { useContext } from "react";
 
 const Landing = () => {
+  const { user } = useContext(UserContext);
+
   return (
     <MainLayout>
-      <section className="landing flex justify-center py-12 md:py-28 xl:py-40 px-4">
+      <section className="landing flex justify-center py-6 md:py-10 xl:py-16">
         <div className="landing__content">
           <h1 className="landing__heading flex flex-wrap gap-y-8 gap-x-4 md:gap-8">
             <span>Collaborate.</span>
             <span>Manage.</span>
             <span className="landing__text">Succeed.</span>
           </h1>
-          <button className="landing__button my-10 rounded-lg px-8 py-4 hover:opacity-90 transition duration-500">
-            Start New Project
-          </button>
+          <Link to={`${user ? '/projects/new' : '/login'}`}>
+            <button className="landing__button my-10 rounded-lg px-8 py-4 hover:opacity-90 transition duration-500">
+              Start New Project
+            </button>
+          </Link>
         </div>
       </section>
     </MainLayout>
--- src/pages/ProjectList/ProjectList.jsx
+++ src/pages/ProjectList/ProjectList.jsx
@@ -1,11 +1,17 @@
 import "./ProjectList.scss";
 import MainLayout from "../../layouts/MainLayout/MainLayout";
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { UserContext } from "../../contexts/userContext";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
+import projectHubApi from "../../utils/projectHubApi";
+import ProjectItem from "../../components/ProjectItem/ProjectItem";
+import Loader from "../../components/Loader/Loader";
 
 const ProjectList = () => {
   const { user } = useContext(UserContext);
+  const [projectList, setProjectList] = useState(null);
+  const [loading, setLoading] = useState(false);
+
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -14,18 +20,52 @@ const ProjectList = () => {
     }
   }, [user, navigate]);
 
+  useEffect(() => {
+    const getProjects = async () => {
+      setLoading(true);
+
+      try {
+        const res = await projectHubApi.getProjects();
+        setProjectList(res.data);
+        setLoading(false);
+      } catch (err) {
+        console.error(err);
+        setLoading(false);
+      }
+    };
+
+    getProjects();
+  }, []);
+
   return (
     <MainLayout>
       <section className="projects">
         <div className="flex flex-wrap gap-2 items-center justify-between">
           <h2 className="projects__heading">Projects</h2>
-          <button className="projects__add-btn rounded-lg px-5 py-1">
-            Start New Project
-          </button>
+          <Link to="/projects/new">
+            <button className="projects__add-btn rounded-lg px-5 py-1">
+              Start New Project
+            </button>
+          </Link>
         </div>
-        <div className="my-10">
-      
-        </div>
+
+        {loading ? (
+          <div className="flex justify-center h-[30vh] py-20">
+            <Loader />
+          </div>
+        ) : (
+          <div className="my-10">
+            {projectList?.length > 0 ? (
+              <>
+                {projectList.map((project) => (
+                  <ProjectItem key={project.id} project={project} />
+                ))}
+              </>
+            ) : (
+              <p>No projects created yet</p>
+            )}
+          </div>
+        )}
       </section>
     </MainLayout>
   );
