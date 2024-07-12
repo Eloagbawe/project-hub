088efe112fb29f35096ded278c24847411b03470
Eloagbawe <eloagbawe@gmail.com>
2024-07-11 22:15:00 -0400

    update main.jsx and global styling

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.scss'
-import { ChakraProvider } from '@chakra-ui/react'
-import { UserProvider } from './contexts/userContext';
-import { AlertProvider } from './contexts/alertContext';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.scss";
+import { ChakraProvider } from "@chakra-ui/react";
+import { UserProvider } from "./contexts/userContext";
+import { AlertProvider } from "./contexts/alertContext";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ChakraProvider>
-      <AlertProvider>
-        <UserProvider>
+      <UserProvider>
+        <AlertProvider>
           <App />
-        </UserProvider>
-      </AlertProvider>
+        </AlertProvider>
+      </UserProvider>
     </ChakraProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
