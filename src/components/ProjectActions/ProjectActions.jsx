a08aabce4073e9832c7729ea216144880460000d
Eloagbawe <eloagbawe@gmail.com>
2024-07-14 10:36:57 -0400

    update project actions component

--- src/components/ProjectActions/ProjectActions.jsx
+++ src/components/ProjectActions/ProjectActions.jsx
@@ -6,8 +6,17 @@ import {
   PopoverTrigger,
 } from "@chakra-ui/react";
 import moreIcon from "../../assets/icons/more.svg";
+import { useContext } from "react";
+import { ProjectContext } from "../../contexts/projectContext";
+import { useNavigate } from "react-router-dom";
 
 const ProjectActions = () => {
+  const { project } = useContext(ProjectContext);
+  const navigate = useNavigate();
+
+  const handleEdit = () => {
+    navigate(`/projects/${project.id}/edit`);
+  }
 
   return (
     <div className="popover">
@@ -21,7 +30,7 @@ const ProjectActions = () => {
         <PopoverContent className="popover__content px-2 pb-2">
           {/* <PopoverCloseButton /> */}
           <div className="mt-3 popover__btn-container rounded-lg px-2">
-            <button className="popover__btn">Edit</button>
+            <button className="popover__btn" onClick={handleEdit}>Edit</button>
           </div>
         </PopoverContent>
       </Popover>
